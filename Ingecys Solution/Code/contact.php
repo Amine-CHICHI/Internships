a90a0ee556902a5e2618b3d777ec09a7d603c245
Amine CHICHI <58734010+xkaz99@users.noreply.github.com>
2021-03-24 16:20:50 +0100

    Add files via upload

---
+++ Ingecys Solution/Code/contact.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INGECYS SOLUTIONS | Agriculture Intelligente</title>
+    <link rel="icon" type="image/png" href="assets/favicon.ico" />
+    <!-- Bootstrap CSS only -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="slick.css" />
+    <!-- AOS CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="style2.css">
+
+
+
+</head>
+
+<body>
+    <!-- Nav Section -->
+    <nav>
+        <div class="logo">
+            <a href="homePage.html">
+                <img src="assets/logo_solutions2.png" alt="brandName">
+            </a>
+        </div>
+        <div class="menu">
+            <ul id='outer-ul'>
+                <li><a href="homePage.html">Accueil</a></li>
+                <li><a href="#">Qui sommes-nous ? <i class="fas fa-caret-down fa-lg"></i></a>
+                    <ul>
+                        <li><a href="IngecysEnBref.html">Ingecys Solutions en bref</a></li>
+                        <li><a href="nosValeur.html">Nos valeurs</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Nos solutions <i class="fas fa-caret-down fa-lg"></i></a>
+                    <ul>
+                        <li><a href="gestion-des-ecoles.html">Gestion des écoles</a></li>
+                        <li><a href="gestion-des-chantiers.html">Gestion des chantiers</a></li>
+                        <li><a href="#">Gestion du transport du personnel (VTC)</a></li>
+                        <li><a href="gestion-des-commandes-et-des-livraisons.html">Gestion des commandes et des livraisons </a></li>
+                        <li><a href="gestion-du-personnel-de-sécurité.html">Système de sécurité</a></li>
+                        <li><a href="agriculture-intelligente.html">Agriculture intelligente</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- End of Nav Section -->
+
+    <!-- Content Section -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <form action="process_Contact.php" class="form-group">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <label for="nom">Nom</label>
+                            <input type="text" id="nom" name="name">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-6" >
+                
+            </div>
+        </div>
+    </div>
+
+
+    <!-- End of Content Section -->
+
+    <!-- Footer Section -->
+    <div class="news-bottom ">
+        <div id="footer" class="container-fluid ">
+            <div class="news-pad ">
+                <div class="row news-bot ">
+                    <div class="col-md-4 news-grid ">
+                        <h3>Get in touch with us</h3>
+                        <div style="color: #fff;margin-top: 20px;font-size: 14px; ">
+                            <address>
+                                    <strong>INGECYS SOLUTIONS</strong><br><br>
+                                    Centre d'Affaires RYAD<br>
+                                    Avenue Arroz, Block U1, Secteur 11<br>
+                                    Hay Ryad - Rabat - Morocco 
+                                </address>
+                            <p>
+                                <i class="fa fa-phone fa-1x "></i> +212 5 37 71 20 71 <br>
+                                <i class="fa fa-fax fa-1x "></i> +212 5 37 71 06 67 <br>
+                                <i class="fa fa-inbox fa-1x "></i> contact@ingecys.com
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 news-grid ">
+                        <h3>Pages</h3>
+                        <ul>
+                            <li><a href="homePage.html ">Accueil</a></li>
+                            <li><a href="IngecysEnBref.html ">Ingecys Solutions en bref</a></li>
+                            <li><a href="NosValeurs.html ">Nos valeurs</a></li>
+                            <li><a href="contact.php ">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 news-grid ">
+                        <h3>Nos Solutions</h3>
+                        <ul>
+                            <li><a href="GestionDesRcoles.html ">Gestion des écoles</a></li>
+                            <li><a href="GestionDesChantiers.html ">Gestion des chantiers</a></li>
+                            <li><a href="GestionDuTransportDuPersonnel.html ">Gestion du transport du personnel (VTC)</a></li>
+                            <li><a href="GestionDescommandesEtDesLivraisons.html ">Gestion des commandes et des livraisons</a></li>
+                            <li><a href="SystèmeDeSécurité.html ">Système de sécurité</a></li>
+                            <li><a href="AgricultureIntelligente.html ">Agriculture intelligente</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Section -->
+
+
+
+
+
+
+
+
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js " integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj " crossorigin="anonymous "></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js " integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous "></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js " integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI " crossorigin="anonymous "></script>
+    <!-- AOS JS -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js "></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
